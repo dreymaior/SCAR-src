4f1891faf095107fe8638563ce0beab46b2be3dc
dreymaior <andrey.maior@gmail.com>
2016-02-01 18:54:12 -0200

    second commit fix some problems

---
+++ dao/SensorDao.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import br.com.caelum.vraptor.ioc.Component;
+import modelo.Sensor;
+
+@Component
+public class SensorDao {
+
+	private final Session session;
+	
+	public SensorDao (Session session) {
+		this.session = session;
+	}
+	
+	public void create(Sensor sensor) {
+		Transaction tx = session.beginTransaction();
+		session.save(sensor);
+		tx.commit();
+	}
+	
+	public Sensor load(Long id){
+		return (Sensor) this.session.load(Sensor.class, id);
+	}
+	
+	public void update(Sensor sensor){
+		Transaction tx = session.beginTransaction();
+		this.session.update(sensor);
+		tx.commit();
+	}
+	
+	public void delete(Sensor sensor){
+		Transaction tx = session.beginTransaction();
+		this.session.delete(sensor);
+		tx.commit();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Sensor> listaTudo() {
+		return (List<Sensor>) this.session.createCriteria(Sensor.class).list();
+	}
+	
+}
